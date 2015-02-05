73cb5021f3259617bb0dd20350eecc89c0c5e3e1
Marcus Veibäck <sirmar@gmail.com>
2015-02-05 20:19:49 +0100

    Main added

---
+++ tetris/main.py
@@ -0,0 +1,13 @@
+"""
+This is the main of this Tetris game.
+"""
+
+class Tetris(object):
+    """Hej"""
+    def __init__(self):
+        """Hej"""
+        pass
+
+    def run(self):
+        """Runs the program"""
+        print "Hej"
