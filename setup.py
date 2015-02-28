85b41ce679ed5d84dcd1ed0b5d3bf3cdefb70217
Marcus Veibäck <sirmar@gmail.com>
2015-02-28 12:45:54 +0100

    New readme

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Setup file for Tetris"""
+
+"""
+Setup file for Tetris.
+"""
 
 from setuptools import setup
 
@@ -8,13 +11,18 @@ setup(
     version="0.1.0",
     author="Marcus Veibäck",
     author_email="sirmar@gmail.com",
+
     packages=["tetris"],
     include_package_data=True,
     url="https://github.com/sirmar/tetris",
+
     # license="LICENSE.txt",
     description="Yes another Tetris.",
-    # long_description=open("README.txt").read(),
+    long_description=open("README.md").read(),
 
-    # Dependent packages (distributions)
-    install_requires=["nose", "coverage", "mock"]
-)
+    install_requires=[
+        "nose",
+        "coverage",
+        "mock",
+        "pylint"
+    ])
